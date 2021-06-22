13d6d224ad8f10e93e634767fbdea4f36ba565e9
Caitlin Swickard <caitlin6029@gmail.com>
2021-06-22 15:20:42 -0600

    Generate markdown for README filled in with data

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link = "";
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -12,8 +19,41 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+  ${badge}
+
+  ##Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * Contribution(#contribution)
+  * [Tests](#tests)
+  * Questions(#questions)
+  
+  ##Installation
+  ${data.installation}
+
+  ##Usage
+  ${data.usage}
+
+  ##License
+  This project is covered under the following license:
+  ${data.license}
+
+  ##Tests
+  ${data.tests}
+
+  ##Contribution
+  ${data.contribution}
+  
+  ##Questions?
+  Reach me here with additional questions:
+  [Github Profile](https://www.github.com/${data.github})
+  ${data.email}
 `;
 }
 
